c61a3dcea3f85c29ca92a5df47b1b863e5b32599
Brianna Bias <brianna.bias@gmail.com>
2021-09-15 11:40:33 -0700

    container finished

--- src/containers/SimpsonsQuote.jsx
+++ src/containers/SimpsonsQuote.jsx
@@ -6,4 +6,31 @@ import { fetchQuotes } from '../services/simpsonsAPI';
 const SimpsonQuote = () => {
     const [quote, setQuote] = useState(null);
     const [loading, setLoading] = useState(false);
-}
+
+    const handleClick = async () => {
+        setLoading(true);
+
+        const quote = fetchQuotes();
+        setQuote(quote);
+        setLoading(false);
+    };
+
+    return (
+        <>
+            <Load onClick={handleClick} />
+            {loading ? (
+                <img src="https://media3.giphy.com/media/DopPsBP7cStO/giphy.gif" alt="bart-loading-spinner" />
+            ) : (
+                quote && (
+                    <Quote
+                        text={quote.text}
+                        character={quote.character}
+                        image={quote.image}
+                    />
+                )
+            )}
+        </>
+    );
+};
+
+export default SimpsonQuote;
